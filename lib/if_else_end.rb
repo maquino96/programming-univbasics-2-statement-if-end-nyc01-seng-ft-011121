2ef6f9d403c3ff68c0f9d83176f1f0203d3331f4
Matthew Aquino <matthewaquino96@gmail.com>
2020-12-27 23:27:54 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,7 +1,7 @@
 # Write your solution here
 
-current_time = Time.now
-current_time = current_time.to_i
+current_time = Time.now.to_i
+#current_time = current_time.to_i
 
 if current_time%2 == 1 
   puts "Odd!"
